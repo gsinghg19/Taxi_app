9d6e474923ed63b53014be6da1dcac51de8b92bf
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-15 12:44:45 +0100

    feat: added navigation for the order screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import MapScreen from "./screens/MapScreen";
+import OrderScreen from "./screens/OrderScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -17,6 +18,7 @@ export default function App() {
           <Stack.Navigator initialRouteName="HomeScreen">
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
             <Stack.Screen name="MapScreen" component={MapScreen} />
+            <Stack.Screen name="OrderScreen" component={OrderScreen} />
           </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,20 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-      <Image
-        style={{ width: 100, height: 100, resizeMode: "contain" }}
-        source={{
-          uri: "https://stock.adobe.com/uk/images/berlin-colored-vector-map/124270591",
-        }}
-      />
-    </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://stock.adobe.com/uk/images/berlin-colored-vector-map/124270591",
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 

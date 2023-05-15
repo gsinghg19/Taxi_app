5963e6a530c7c18dd3b4647a223b456bd69c02f9
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-15 16:04:36 +0100

    feat: added working gooogle places autocomplete text input on teh home screen. Places APi data is  now being fetched form gcp api when a user inputs a location in the search box.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
+import { Google_Map_Api_Key } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const HomeScreen = () => {
   return (
@@ -11,6 +13,29 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{
             uri: "https://as1.ftcdn.net/v2/jpg/03/51/17/28/1000_F_351172814_sX7DmvFaLYnj6E2iCXhH8aj51rAxJKS2.jpg",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where are you?"
+          minLength={2}
+          autoFocus={false}
+          returnKeyType={"default"}
+          fetchDetails={true}
+          styles={{
+            container: {
+              flex: 0,
+              textInput: {
+                fontSize: 18,
+              },
+            },
+          }}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: Google_Map_Api_Key,
+            language: "en",
           }}
         />
         <NavOptions />

fb4f49a11fda08851bbda2874426fbeb3bfad125
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-15 22:12:01 +0100

    update: added google places autocomplete, as a seperate component and imported and rendered this component on the homescreen. Cleaner this way.

---
+++ components/GoogleAutoComplete.js
@@ -0,0 +1,44 @@
+import { useDispatch } from "react-redux";
+import React from "react";
+import { Google_Map_Api_Key } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { setDestination, setOrigin } from "../slices/navSlice";
+
+const GoogleAutoComplete = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <GooglePlacesAutocomplete
+      placeholder="Where too?"
+      minLength={2}
+      autoFocus={false}
+      returnKeyType={"search"}
+      fetchDetails={true}
+      styles={{
+        container: {
+          flex: 0,
+          textInput: {
+            fontSize: 18,
+          },
+        },
+      }}
+      onPress={(data, details = null) => {
+        dispatch(
+          setOrigin({
+            location: details.geometry.location,
+            description: data.description,
+          })
+        );
+        dispatch(setDestination(null));
+        //console.log(data);
+      }}
+      query={{
+        key: Google_Map_Api_Key,
+        language: "en",
+      }}
+      debounce={400}
+      enablePoweredByContainer={false}
+    />
+  );
+};
+export default GoogleAutoComplete;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,10 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import { View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
-import { Google_Map_Api_Key } from "@env";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { useDispatch } from "react-redux";
-import { setDestination, setOrigin } from "../slices/navSlice";
+import GoogleAutoComplete from "../components/GoogleAutoComplete";
 
 const HomeScreen = () => {
-  const dispatch = useDispatch();
-
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -19,36 +14,7 @@ const HomeScreen = () => {
             uri: "https://as1.ftcdn.net/v2/jpg/03/51/17/28/1000_F_351172814_sX7DmvFaLYnj6E2iCXhH8aj51rAxJKS2.jpg",
           }}
         />
-        <GooglePlacesAutocomplete
-          placeholder="Where too?"
-          minLength={2}
-          autoFocus={false}
-          returnKeyType={"search"}
-          fetchDetails={true}
-          styles={{
-            container: {
-              flex: 0,
-              textInput: {
-                fontSize: 18,
-              },
-            },
-          }}
-          onPress={(data, details = null) => {
-            dispatch(
-              setOrigin({
-                location: details.geometry.location,
-                description: data.description,
-              })
-            );
-            dispatch(setDestination(null));
-          }}
-          query={{
-            key: Google_Map_Api_Key,
-            language: "en",
-          }}
-          debounce={400}
-          enablePoweredByContainer={false}
-        />
+        <GoogleAutoComplete />
         <NavOptions />
       </View>
     </SafeAreaView>

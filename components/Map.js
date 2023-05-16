7a7df793a77a230f6705cc84cce5e1837ff38731
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-16 14:56:46 +0100

    feat: map feature now builds app on the selected location given.

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,23 @@
 import React from "react";
 import MapView from "react-native-maps";
+import { useSelector } from "react-redux";
 import tw from "twrnc";
+import { selectOrigin } from "../slices/navSlice";
 
 const Map = () => {
+  const origin = useSelector(selectOrigin);
+
   return (
     <MapView
       mapType="mutedStandard"
       style={tw`flex-1`}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }}
-    />
+    ></MapView>
   );
 };
 

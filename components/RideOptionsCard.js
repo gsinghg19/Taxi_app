3109f2c6d6ae8e4e9cb6034e9d4ff5e714c2c1ba
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-18 17:34:32 +0100

    fix: alteration to styling for font placeholder on ride options screen.

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
 
 const RideOptionsCard = () => {
   return (
-    <View>
+    <SafeAreaView style={tw`pl-5 pt-2 font-600 bg-gray-400`}>
       <Text>RideOptionsCard</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 

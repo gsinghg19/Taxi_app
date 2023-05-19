748681e5eecdad80ca8b36fbb3d89e20ad915ded
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-19 12:22:20 +0100

    feature: added destination marker functionality and the ability for te map to zoom out and show the whole map and direction to be taken on the map.

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
 import tw from "twrnc";
@@ -9,10 +9,25 @@ import { Google_Map_Api_Key } from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: {
+        top: 50,
+        bottom: 50,
+        right: 50,
+        left: 50,
+      },
+    });
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       mapType="mutedStandard"
+      loadingEnabled="true"
       style={tw`flex-1`}
       initialRegion={{
         latitude: origin.location.lat,
@@ -42,26 +57,24 @@ const Map = () => {
             latitude: origin.location.lat,
             longitude: origin.location.lng,
           }}
-          title="Your starting point"
+          title="Starting point"
           description={origin.description}
           identifier="origin"
         />
       )}
 
-      {/*================================================== UNDER CONSTRUCTION ========================= */}
-      {/* TODO: fix destination pin. clikcing marker deosnt give destination details. it closes the app. */}
-      {/* {destination?.location && (
+      {destination?.location && (
         <Marker
+          draggable
           coordinate={{
             latitude: destination.location.lat,
             longitude: destination.location.lng,
           }}
-          title="Destination"
-          description={destination.location}
+          title="Drestination"
+          description={destination.description}
           identifier="destination"
         />
-      )} */}
-      {/*================================================== UNDER CONSTRUCTION ========================= */}
+      )}
     </MapView>
   );
 };

80397a194afc8cbcf18c2419ea8e8b7f14b5ec7b
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-19 15:18:00 +0100

    update: altering navSlice to have the global state of the app contain details for home, work and two fav destinations.

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -4,6 +4,10 @@ const initialState = {
   origin: null,
   destination: null,
   travelTimeInformation: null,
+  home: null,
+  work: null,
+  favOne: null,
+  favTwo: null,
 };
 
 export const navSlice = createSlice({
@@ -19,17 +23,40 @@ export const navSlice = createSlice({
     setTravelTimeInformation: (state, action) => {
       state.travelTimeInformation = action.payload;
     },
+    setHome: (state, action) => {
+      state.home = action.payload;
+    },
+    setWork: (state, action) => {
+      state.work = action.payload;
+    },
+    setFavOne: (state, action) => {
+      state.favOne = action.payload;
+    },
+    setFavTwo: (state, action) => {
+      state.favTwo = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setOrigin, setDestination, setTravelTimeInformation } =
-  navSlice.actions;
+export const {
+  setOrigin,
+  setDestination,
+  setTravelTimeInformation,
+  setHome,
+  setWork,
+  setFavOne,
+  setFavTwo,
+} = navSlice.actions;
 
 //selectors
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
 export const selectTravelTimeInformation = (state) =>
   state.nav.travelTimeInformation;
+export const selectHome = (state) => state.nav.home;
+export const selectWork = (state) => state.nav.work;
+export const selectFavOne = (state) => state.nav.favOne;
+export const selectFavTwo = (state) => state.nav.favTwo;
 
 export default navSlice.reducer;
